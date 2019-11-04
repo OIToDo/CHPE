c5ea89a1801a02fec1ff68ede60c4b514ae0925f
Gianetti <gianluca.piccardo@student.hu.nl>
2019-11-04 14:24:44 +0100

    kleurtjes 12
    k

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -25,7 +25,7 @@ public class MyGdxGame implements ApplicationListener {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1,0,0,1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
